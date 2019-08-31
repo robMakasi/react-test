999ee5c4b51bc6460b3f0a47441753d166b59548
robMakasi <robert0makasi@gmail.com>
2019-08-31 16:02:41 +1100

    Updates

---
+++ precache-manifest.b4444abc102da1ad82614993acb12282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc5679895d948debdce7001059b96b0",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "1581232734b43e1639f7",
+    "url": "/react-test/static/css/main.1c1b7591.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/react-test/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "1581232734b43e1639f7",
+    "url": "/react-test/static/js/main.93e31931.chunk.js"
+  },
+  {
+    "revision": "177a9aee4122cc056c32",
+    "url": "/react-test/static/js/runtime~main.c2cfb6ba.js"
+  }
+]);
